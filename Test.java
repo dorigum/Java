08e9c10fe4af6492c2f74476919164a3f32548c1
doyeon <kkamang03@gmail.com>
2021-03-17 21:30:20 +0900

    java test

---
+++ Test.java
@@ -0,0 +1,34 @@
+// 클래스명과 동일한 파일명 생성
+// 대소문자 구분
+// 클래스 이름은 대문자로 시작
+// 입력한 클래스 이름으로 java 파일 생성
+// 클래스이름.java
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("test 입니다.");
+		// 화면에 "" 안의 문장을 출력하고 다음 행을 이동
+		
+		// 주석 처리 : Ctrl + /
+		// 주석 해제 : Ctrl + / 한번 더
+		// 주석 묶음 처리 : Ctrl + Shift + /
+		// 주석 묶음 처리 해제 : Ctrl + Shift + \
+		
+		/*
+		 * System.out.println("test 입니다."); System.out.println("test 입니다.");
+		 * System.out.println("test 입니다.");
+		 */
+		//블록 주석 처리 시 자동 정렬 해제
+		// Window / Preferences
+		// Java / Code Style / Formater
+		// Edit / New : 새 이름 입력
+		// Comments : Enable block comment formatting
+		
+		System.out.println("test 입니다.");
+		System.out.println("test 입니다.");
+		System.out.println("test 입니다.");
+
+	}
+}
+
+// Ctrl + S : 파일 저장
